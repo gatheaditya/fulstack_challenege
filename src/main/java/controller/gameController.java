66ff5af27b2ea9c66a9d3a0ef5b9a2bfa0d70530
gatheaditya <gatheaditya@gmail.com>
2018-02-18 23:04:57 +0530

    added pagination

--- src/main/java/controller/gameController.java
+++ src/main/java/controller/gameController.java
@@ -2,6 +2,7 @@ package controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,5 +37,12 @@ public class gameController {
 	public @ResponseBody Iterable<gameModel> fetch()
 	{
 		return cr.findAll();
+		
+	}
+	@GetMapping(path="/limit")
+	@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+	public @ResponseBody Iterable<gameModel> limitedFetch()
+	{
+		return cr.findAllByname(new PageRequest(0,20));
 	}
 }
--- src/main/java/controller/gameRepository.java
+++ src/main/java/controller/gameRepository.java
@@ -1,7 +1,15 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-public interface gameRepository  extends CrudRepository<gameModel,Long> {
+public interface gameRepository  extends CrudRepository<gameModel,Long>  {
+	
+	List<gameModel> findAllByname(Pageable Pageable);
+	
+
+	
 
 }
