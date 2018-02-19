9dbe17229dd37d828e8546d7935bb9e872587220
gatheaditya <gatheaditya@gmail.com>
2018-02-19 17:26:46 +0530

    allowed all/1

--- src/main/java/controller/WebSecurityConfig.java
+++ src/main/java/controller/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests().antMatchers("/all")
+            .authorizeRequests().antMatchers("/all","all/1")
             .permitAll()
             .anyRequest().authenticated()
                 .and()
