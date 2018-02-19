3edc028970ff152fdb5c19b8d94a8534e6ad3124
gatheaditya <gatheaditya@gmail.com>
2018-02-19 18:25:12 +0530

    sorted the array

--- src/main/java/controller/gameController.java
+++ src/main/java/controller/gameController.java
@@ -8,6 +8,7 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,7 +42,7 @@ public class gameController {
 	@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 	public @ResponseBody Page<gameModel> fetch(@PathVariable("pageid") int pageid)
 	{
-		return cr.findAll(new PageRequest(pageid,10));
+		return cr.findAll(new PageRequest(pageid,10,Direction.ASC,"title"));
 		
 	}
 	
