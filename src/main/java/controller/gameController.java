1ca04f94b4c555242bf7bdea2dfc5a00794fd0c4
gatheaditya <gatheaditya@gmail.com>
2018-02-19 16:07:27 +0530

    added pagination

--- src/main/java/controller/gameController.java
+++ src/main/java/controller/gameController.java
@@ -1,10 +1,16 @@
 package controller;
 
 
+
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,34 +21,34 @@ public class gameController {
 	@Autowired
 	private gameRepository cr;
 	
+	
+	
+	
 	@GetMapping(path="/")	
 	public@ResponseBody String greeting()
 	{
 		return "Welcome to game REST  ";
 	}
 	
-//	@GetMapping(path="/add")	
-//	public @ResponseBody String  add()
-//	{
-//		
-//		gameModel cm = new gameModel();		
-//		cm.setUsername("aditya");
-//		cm.setPassword("password");
-//		cr.save(cm);
-//		return "added";
-//	}
 
 	@GetMapping(path="/all")
 	@CrossOrigin(origins = "https://gatheaditya.github.io", maxAge = 3600)
-	public @ResponseBody Iterable<gameModel> fetch()
+	public @ResponseBody Iterable<gameModel> fetchall()
 	{
 		return cr.findAll();
+		}
+	@GetMapping(path="/all/{pageid}")
+	@CrossOrigin(origins = "https://gatheaditya.github.io", maxAge = 3600)
+	public @ResponseBody Page<gameModel> fetch(@PathVariable("pageid") int pageid)
+	{
+		return cr.findAll(new PageRequest(pageid,20));
 		
 	}
-	@GetMapping(path="/limit")
-	@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
-	public @ResponseBody Iterable<gameModel> limitedFetch()
-	{
-		return cr.findAllByname(new PageRequest(0,20));
-	}
+	
+//	@GetMapping(path="/platforms")
+//	@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+//	public @ResponseBody List<gameModel> Platforms()
+//	{
+//		//return cr.findDistinctAllByPlatform();
+//	}
 }
--- src/main/java/controller/gameRepository.java
+++ src/main/java/controller/gameRepository.java
@@ -1,13 +1,21 @@
 package controller;
 
+
+
+
+
+
+
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-public interface gameRepository  extends CrudRepository<gameModel,Long>  {
-	
-	List<gameModel> findAllByname(Pageable Pageable);
+public interface gameRepository  extends CrudRepository<gameModel,Integer>  {
+
+	List<gameModel> findByPlatform(String genre,Pageable pageable);
+	Page<gameModel> findAll(Pageable pageable);
 	
 
 	
