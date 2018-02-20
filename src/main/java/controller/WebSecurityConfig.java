760ed81332fa37ee8a95f7d340d0e42f1fb8a2ef
gatheaditya <gatheaditya@gmail.com>
2018-02-20 19:42:19 +0530

    allow get

--- src/main/java/controller/WebSecurityConfig.java
+++ src/main/java/controller/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests().antMatchers("/all","/all/*")
+            .authorizeRequests().antMatchers("/all","/all/*","/get/*")
             .permitAll()
             .anyRequest().authenticated()
                 .and()
