7df302959b4ef3e611a9641e45eff0ff1c50a7a7
gatheaditya <gatheaditya@gmail.com>
2018-02-20 18:50:57 +0530

    j

--- src/main/java/controller/gameController.java
+++ src/main/java/controller/gameController.java
@@ -8,6 +8,7 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,7 +39,7 @@ public class gameController {
 		return cr.findAll();
 		}
 	@GetMapping(path="/all/{pageid}")
-	@CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
+	@CrossOrigin(origins = "https://gatheaditya.github.io", maxAge = 3600)
 	public @ResponseBody Page<gameModel> fetch(@PathVariable("pageid") int pageid)
 	{
 		return cr.findAll(new PageRequest(pageid,10));
