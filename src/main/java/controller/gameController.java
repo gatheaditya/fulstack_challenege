6292e3674ca1f5239bc887c0d375bcdee7074ded
gatheaditya <gatheaditya@gmail.com>
2018-02-20 18:46:08 +0530

    switched to localhost

--- src/main/java/controller/gameController.java
+++ src/main/java/controller/gameController.java
@@ -8,7 +8,6 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,7 +38,7 @@ public class gameController {
 		return cr.findAll();
 		}
 	@GetMapping(path="/all/{pageid}")
-	@CrossOrigin(origins = "https://gatheaditya.github.io", maxAge = 3600)
+	@CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
 	public @ResponseBody Page<gameModel> fetch(@PathVariable("pageid") int pageid)
 	{
 		return cr.findAll(new PageRequest(pageid,10));
